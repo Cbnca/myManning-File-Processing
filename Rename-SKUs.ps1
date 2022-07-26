7ab93bbe5447a28df4741850d3eace493bd0563f
Cbnca <GitHub@cabonca.com>
2022-07-26 23:02:57 +0100

    get child

---
+++ Rename-SKUs.ps1
@@ -0,0 +1,18 @@
+Get-Verb
+# C:\Users\neilp\VSCodeWorkSpace\Cbnca-file-processing-lp\images
+$SourceDirectory =  'C:\Users\neilp\VSCodeWorkSpace\Cbnca-file-processing-lp\images'
+
+Get-ChildItem -Path $SourceDirectory | fo
+
+$x="kjdofjhio["
+$x.GetType()
+
+
+
+
+$_ -match "([0-9]{6}])"
+
+
+
+
+Where-Object {$_.Name -eq "msedge"} 
