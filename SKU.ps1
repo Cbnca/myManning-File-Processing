d4f9cfbf0eb68c2d99f1321d70b5a6a89cd4b18f
Cbnca <GitHub@cabonca.com>
2022-07-26 22:24:43 +0100

    functions

--- SKU.ps1
+++ SKU.ps1
@@ -9,5 +9,30 @@ $Matches[0]
 # enough ammo to find the SKU within a file name
 "10.0.0.12 - PC1" -match "([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})"
 "10.0.0.12 - PC1" -match "([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.(?<last>[0-9]{1,3})"
+#"( [0-9] {1,3} )    \.    ( [0-9] {1,3} )    \.    ( [0-9] {1,3} )    \.    ( ?<last> [0-9] {1,3} )"
+$Matches.last
+
+function Get-HelloWorld{"Hello World"}
+Get-HelloWorld
+
+function Get-Greeting {
+    param (
+        [string]$USerName
+        )
+    "Hello World to " + $USerName
+}
+
+Get-Greeting "You"
 
 
+
+function Add-Integers {
+    param (
+        [Int32]$firstInteger,
+        [Int32]$secondInteger
+        )
+    $firstInteger + $secondInteger
+}
+
+$Result = Add-Integers 2 3
+Write-Host " [Result] $($Result)"
