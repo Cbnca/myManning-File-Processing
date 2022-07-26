34d52e3b5806ebee14bbd87f4ebff9b0d151872e
Cbnca <GitHub@cabonca.com>
2022-07-26 21:40:32 +0100

    a

--- SKU.ps1
+++ SKU.ps1
@@ -6,7 +6,7 @@ Get-Process | Where-Object {$_.Name -eq "msedge"} | Stop-Process  -WhatIf
 $Matches[0]
 
 "ABCDEF" -match "\w{3}"
-
+# enough ammo to find the SKU within a file name
 "10.0.0.12 - PC1" -match "([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})"
 "10.0.0.12 - PC1" -match "([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.(?<last>[0-9]{1,3})"
 
