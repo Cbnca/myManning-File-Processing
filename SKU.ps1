cbcfdd08086652197f25b13725995b68b6bda879
Cbnca <GitHub@cabonca.com>
2022-07-26 21:37:53 +0100

    regExp

---
+++ SKU.ps1
@@ -0,0 +1,13 @@
+Get-Process | Where-Object {$_.Name -eq "msedge"} | Stop-Process  -WhatIf
+
+"C#, PowerShell, VBScript, Python, and C++" -match "PowerShell"
+
+
+$Matches[0]
+
+"ABCDEF" -match "\w{3}"
+
+"10.0.0.12 - PC1" -match "([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})"
+"10.0.0.12 - PC1" -match "([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.(?<last>[0-9]{1,3})"
+
+
