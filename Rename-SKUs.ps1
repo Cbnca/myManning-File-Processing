3f10cae5b69d11a1e7dbe6a9c40104a14bf9ebbc
Cbnca <GitHub@cabonca.com>
2022-07-26 23:16:20 +0100

    effectively filters

--- Rename-SKUs.ps1
+++ Rename-SKUs.ps1
@@ -2,7 +2,7 @@ Get-Verb
 # C:\Users\neilp\VSCodeWorkSpace\Cbnca-file-processing-lp\images
 $SourceDirectory =  'C:\Users\neilp\VSCodeWorkSpace\Cbnca-file-processing-lp\images'
 
-Get-ChildItem -Path $SourceDirectory | fo
+Get-ChildItem -Path $SourceDirectory -filter *.jpg   | Where Name -match "^[0-9]{6}"
 
 $x="kjdofjhio["
 $x.GetType()
